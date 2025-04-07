f3835ec19ef9cd2fcec1d7c6b9126ce2f8874e8e
Martins Greizis <grei0003@hz.nl>
2025-04-07 14:55:39 +0200

    UnrealTesting schema rename

--- src/app/models/UnrealTesting.ts
+++ src/app/models/UnrealTesting.ts
@@ -1,13 +1,15 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface IUnrealTesting extends Document {
-  name: string;
+  // name: string;
+  mesh_name: string;
   value?: string;
   createdAt?: Date;
 }
 
 const UnrealTestingSchema = new Schema<IUnrealTesting>({
-  name: { type: String, required: true },
+  // name: { type: String, required: true },
+  mesh_name: { type: String, required: true },
   value: { type: String, required: false },
   createdAt: { type: Date, required: false, default: Date.now },
 });
